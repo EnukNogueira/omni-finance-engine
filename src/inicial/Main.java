eb458d03f48e062ff3727ea4da93e8bae5dbe5b7
Enuk <enuk.santos@gmail.com>
2026-06-22 15:44:33 -0300

    criando a main

--- src/inicial/Main.java
+++ src/inicial/Main.java
@@ -1,17 +1,13 @@
 package inicial;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    acao minhaAcao = new acao(1, "Banco do Brasil","BBAS3",5000.0,100,55.55);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    rendaFixa meuCdb = new rendaFixa(2,"Itau","CDB_ITAU",10000.0, 2, 0.10);
+
+    System.out.println("---Relatório de investimentos---");
+    System.out.println("Ativo: " + minhaAcao.getCodigo() +" Valor atual: R$" + minhaAcao.calcularValorAtual());
+    System.out.println("Renda fixa: " + meuCdb.getCodigo()+ " Valor atual: R$" + meuCdb.calcularValorAtual());
     }
 }
